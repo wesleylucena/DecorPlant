239881b5433692cad0cb30375ff6a0fe99a57dd2
wesleylucena <61255166+wesleylucena@users.noreply.github.com>
2021-11-24 23:14:22 -0300

    Add files via upload

---
+++ PlantasDecorativas/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlantasDecorativas
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_ClickedAsync(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Planta1());
+        }
+    }
+}
